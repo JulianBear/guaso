c8e003196a32ca3880c158adf56e81a2dac6e340
rinstartoooo <1301988295@qq.com>
2023-07-04 15:07:57 +0800

    lfc board

---
+++ src/Page/LFCBoard.vue
@@ -0,0 +1,16 @@
+
+  <a-list 
+  item-layout="horizontal"
+  :grid="{ gutter: 16, xs: 1, sm: 2, md: 4, lg: 4, xl: 6, xxl: 3 }"
+  :data-source="props.pictureList">
+
+ 
+
+</a-list>
+ 
+
+
+<script setup lang="ts">
+</script>
+<style scoped>
+</style>
